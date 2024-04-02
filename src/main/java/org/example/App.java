07a65f89d5c3f9269a2666411d10fd2f2e573d16
Gleb <Gleb.by2005@gmail.com>
2024-04-02 14:40:42 +0300

    added files

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Gleb!" );
+        System.out.println("LABA 12");
         Tester.staticMethod();
     }
 }
