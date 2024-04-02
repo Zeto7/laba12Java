3b1214b89d18f648aaaefd86ba3d51cb4a2ce139
Gleb <Gleb.by2005@gmail.com>
2024-04-02 15:12:13 +0300

    added debug

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Gleb!" );
+        System.out.println("DEBG");
         Tester.staticMethod();
     }
 }
